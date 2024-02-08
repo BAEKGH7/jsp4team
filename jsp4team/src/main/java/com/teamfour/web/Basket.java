63c6ddce5207812fa8f5baeddf3000eeea5d8307
Ljeelra <dlwldms9803@gmail.com>
2024-02-08 16:01:12 +0900

    장바구니 버튼 새로고침해도 유지

--- jsp4team/src/main/java/com/teamfour/web/Basket.java
+++ jsp4team/src/main/java/com/teamfour/web/Basket.java
@@ -1,6 +1,7 @@
 package com.teamfour.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -26,20 +27,25 @@ public class Basket extends HttpServlet {
 		HttpSession session = request.getSession();
 		List<CartDTO> list = null;
 		List<CartDTO> list2 = null;
+		int totalprice=0;
+
 		if(session.getAttribute("mid") == null|| session.getAttribute("mname") == null) {
 			 response.sendRedirect("./login");
 		}else{
 			BasketDAO dao = new BasketDAO();
 			CartDTO dto = new CartDTO();
 			int mno=(int) session.getAttribute("mno");
+			
 			dto.setMno(mno);
 			list=dao.cartList(mno);
-			/*
-			 * int choice=(int) request.getAttribute("choice"); list2=dao.priceList(mno,
-			 * choice );
-			 */
-			request.setAttribute("list", list);
+			list2=dao.cartList2(mno);
 			
+			totalprice = dao.sumprice(mno);
+			System.out.println("총액: "+totalprice);
+
+			request.setAttribute("list", list);
+			request.setAttribute("list2", list2);
+			request.setAttribute("totalprice",totalprice);
 			
 			RequestDispatcher rd = request.getRequestDispatcher("basket.jsp");
 			rd.forward(request, response);
@@ -48,12 +54,41 @@ public class Basket extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getParameter("mid"));
-		System.out.println(request.getParameter("isbn"));
-	//	String mid = request.getParameter("mid");
-	//	String isbn = request.getParameter("isbn");
+		//System.out.println(request.getParameter("isbn"));
+		//System.out.println(request.getParameter("ccheck"));
+		HttpSession session = request.getSession();
+		int mno = (int) session.getAttribute("mno");
+		String isbn = request.getParameter("isbn");
+		String ccheck = request.getParameter("ccheck");
 		
-	//	CartDTO dto = 
+		CartDTO dto = new CartDTO();
+		dto.setMno(mno);
+		dto.setIsbn(isbn);
+		dto.setCcheck(ccheck);
+		
+		BasketDAO dao = new BasketDAO();
+		int totalprice=0;
+		if(isbn == null || isbn == "") {
+			int result = dao.allUpdateCcheck(dto);
+			if(result==1) {
+				totalprice = dao.sumprice(dto);
+				//request.setAttribute("totalprice", totalprice);
+				System.out.println("1 : "+totalprice);
+				PrintWriter pw = response.getWriter(); pw.print(totalprice);				
+			} else {
+				System.out.println("업데이트 된 ccheck 수 : "+result);
+				totalprice = dao.sumprice(dto);
+				//request.setAttribute("totalprice", totalprice);
+				System.out.println("2 : "+totalprice);
+				PrintWriter pw = response.getWriter(); pw.print(totalprice);
+			}
+		} else {
+			int result = dao.updateCcheck(dto);
+			totalprice = dao.sumprice(dto);
+			System.out.println("3 : "+totalprice);
+			PrintWriter pw = response.getWriter(); pw.print(totalprice);
+		}		
 	}
+	
 
 }
